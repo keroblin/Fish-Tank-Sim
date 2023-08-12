dda89989d8a90963ff1aa4573e2ddeefb9634048
Cas <keroblinco@gmail.com>
2023-08-12 17:19:18 +0100

    made camera move more efficient and ignore the first side of the tank it hits

--- Assets/Scripts/CameraMove.cs
+++ Assets/Scripts/CameraMove.cs
@@ -8,63 +8,69 @@ public class CameraMove : MonoBehaviour
     //I want to make it slerp nicely into position using a public curve to control the movement
     //i also want to use quaternions instead of eulerangles
 
-    public Collider front;
-    public Collider back;
-    public Collider left;
-    public Collider right;
-    public Collider top;
-    public Collider bottom;
+    public GameObject front;
+    public GameObject back;
+    public GameObject left;
+    public GameObject right;
+    public GameObject top;
+    public GameObject bottom;
 
+    List<GameObject> sides = new List<GameObject>();
+
+    Camera cam;
     Vector3 initRot;
     Vector3 currentTarget;
+    GameObject face;
     bool isUp = false;
     void Start()
     {
+        cam = Camera.main;
         initRot = transform.eulerAngles;
+        sides.Add(front);
+        sides.Add(back);
+        sides.Add (left);
+        sides.Add(right);
+        sides.Add(top);
+        sides.Add(bottom);
+
+        face = front;
     }
 
-    public void MoveUp()
+    public void Rot(int dir)
     {
-        if (!isUp)
+        switch (dir)
         {
-            transform.Rotate(90,0,0);
-            isUp = true;
+            case 1:
+            case 2:
+                if (isUp)
+                {
+                    transform.Rotate(0, 0, 45 * dir);
+                }
+                else
+                {
+                    transform.Rotate(0, 45 * dir, 0);
+                }
+                break;
+            case 3:
+                if (!isUp)
+                {
+                    transform.Rotate(90, 0, 0);
+                    SetClickthrough();
+                    isUp = true;
+                }
+                break;
+            case 4:
+                if (isUp)
+                {
+                    transform.Rotate(-90, 0, 0);
+                    isUp = false;
+                }
+                break;
         }
-    }
-    public void MoveDown()
-    {
-        if (isUp)
-        {
-            transform.Rotate(-90, 0,0);
-            isUp = false;
-        }
-    }
-
-    public void RotateSide(int dir)
-    {
-        /*if(transform.eulerAngles.y + (45*dir) > 360)
-        {
-
-            transform.Rotate(transform.eulerAngles.x, 0, transform.eulerAngles.z);
-        }
-        else if (transform.eulerAngles.y + (45*dir) < 0)
-        {
-            transform.Rotate(transform.eulerAngles.x, 360, transform.eulerAngles.z);
-        }
-        currentTarget = new Vector3(transform.eulerAngles.x,transform.eulerAngles.y + (90*dir),transform.eulerAngles.z);*/
-        if (isUp)
-        {
-            transform.Rotate(0, 0, 45 * dir);
-        }
-        else
-        {
-            transform.Rotate(0, 45 * dir, 0);
-        }
-        //StopAllCoroutines();
-        //ProgressRotation(pos);
+        SetClickthrough();
     }
 
-    /*IEnumerator ProgressRotation(Vector3 target)
+   /* IEnumerator ProgressRotation(Vector3 target)
     {
         while(transform.eulerAngles.magnitude > target.magnitude+.1f)
         {
@@ -74,4 +80,33 @@ public class CameraMove : MonoBehaviour
         }
         transform.Rotate(target);
     }*/
+
+    void SetClickthrough()
+    {
+        face.layer = 9;
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+        int layerMask = (1 << 9); //tank only
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity, layerMask))
+        {
+            face = hit.collider.gameObject;
+            Debug.Log("Did Hit");
+        }
+        else
+        {
+            Debug.Log("Did not Hit");
+        }
+
+        foreach (GameObject go in sides)
+        {
+            if(go != face)
+            {
+                go.layer = 9;
+            }
+            else
+            {
+                go.layer = 2;
+            }
+        }
+    }
 }
